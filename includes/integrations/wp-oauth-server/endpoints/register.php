0d1cf2ab52c153595f558fc6fc36ac73cfc086c4
kjohnson <me@kylebjohnson.me>
2017-05-17 12:29:29 -0400

    add client creation.

--- includes/integrations/wp-oauth-server/endpoints/register.php
+++ includes/integrations/wp-oauth-server/endpoints/register.php
@@ -47,9 +47,69 @@ final class NF_Auth_Integrations_WPOAuthServer_Endpoints_Register
             return;
         }
 
-        // TODO: Create Client and return client_id
+        $site_url = parse_url( $client_redirect );
+
+        $client_insert = $this->create_client( array(
+            'secret' => $client_secret,
+            'name' => $site_url[ 'host' ]
+        ));
+
+        $client_id = get_post_meta( $client_insert, 'client_id', /* single */ true );
+
+        $client_redirect = add_query_arg( 'client_id', $client_id, $client_redirect );
+
         wp_redirect( $client_redirect );
         exit();
     }
 
+    /**
+     * @param null $client_data
+     * @return bool|int $client_insert
+     */
+    private function create_client( $client_data = null )
+    {
+        $client_data = array_merge( array(
+            'name' => '',
+            'secret' => '',
+            'grant_types' => array(),
+            'redirect_uri' => '',
+            'user_id' => '',
+            'scope' => ''
+        ), $client_data );
+
+        if ( empty( $client_data[ 'secret' ] ) ) {
+            exit( 'Client secret not found.' );
+            return false;
+        }
+
+        do_action( 'wo_before_create_client', array( $client_data ) );
+
+        $client_id     = wo_gen_key();
+        $client_secret = $client_data[ 'secret' ]; // TODO: Sanatize this.
+
+        $client = array(
+            'post_title'     => wp_strip_all_tags( $client_data[ 'name' ] ),
+            'post_status'    => 'publish',
+            'post_author'    => get_current_user_id(),
+            'post_type'      => 'wo_client',
+            'comment_status' => 'closed',
+            'meta_input'     => array(
+                'client_id'     => $client_id,
+                'client_secret' => $client_secret,
+                'grant_types'   => $client_data[ 'grant_types' ],
+                'redirect_uri'  => $client_data[ 'redirect_uri' ],
+                'user_id'       => $client_data[ 'user_id' ],
+                'scope'         => $client_data[ 'scope' ]
+            )
+
+        );
+
+        // Insert the post into the database
+        $client_insert = wp_insert_post( $client );
+        if ( is_wp_error( $client_insert ) ) {
+            exit( $client_insert->get_error_message() );
+        }
+
+        return $client_insert;
+    }
 }
