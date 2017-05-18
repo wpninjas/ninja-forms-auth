37251a9b55ce9bd0b0d1e3eaf405ce2f9ff57d96
kjohnson <me@kylebjohnson.me>
2017-05-18 14:58:00 -0400

    add example endpoints.

--- includes/auth.php
+++ includes/auth.php
@@ -7,6 +7,27 @@ final class NF_Auth extends NF_Auth_Plugin
         parent::__construct( $version, $file );
 
         (new NF_Auth_Integrations_WPOAuthServer_Endpoints_Register())->init();
+
+        add_action( 'rest_api_init', function () {
+            register_rest_route( 'ninja-forms-auth/v1', '/example-subscriber', array(
+                'methods' => 'GET',
+                'callback' => function(){
+                    return [ 'current_user_can' => 'read' ];
+                },
+                'permission_callback' => function () {
+                    return current_user_can( 'read' );
+                }
+            ) );
+            register_rest_route( 'ninja-forms-auth/v1', '/example-admin', array(
+                'methods' => 'GET',
+                'callback' => function(){
+                    return [ 'current_user_can' => 'manage_options' ];
+                },
+                'permission_callback' => function () {
+                    return current_user_can( 'manage_options' );
+                }
+            ) );
+        } );
     }
 
     /*
